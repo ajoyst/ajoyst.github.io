bd35ac912c18496c33889e1d03481b190acd98d0
Joy\Joy Stephens <alice.joystephens@gmail.com>
2020-07-20 16:39:46 -0500

    Created 1st blog post with CSS formatting. No JS.

---
+++ README.md
@@ -0,0 +1,3 @@
+The goal of this project is to learn how to format something a website like a blog.
+
+Isn't actually ipsum. I'm going to aim for some blue and orange colors. 
---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("java Script Is Connected");
+
+function isThereCheese(muchCheese) {
+  if(muchCheese) { return "Woo hoo!";}
+  return "Ah man.";
+}
+console.log(isThereCheese(true));
